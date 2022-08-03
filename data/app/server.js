32712b524757bda70d3217c1510974307d2abea8
Jai chouhan <92376924+Jaichouhan@users.noreply.github.com>
2022-08-03 15:25:18 +0530

    routing html add

--- data/app/server.js
+++ data/app/server.js
@@ -1,23 +1,55 @@
-const http = require("http");
-const fs = require("fs");
-const port = 5000;
+// const http = require("http");
+const express = require("express");
+const app = express();
+// const fs = require("fs");
+const port = 8000;
 const hostname = "localhost";
 
-const server = http.createServer((req, res) => {
-  res.setHeader("Content-Type", "text/html");
-  //   res.write("<h1>hello world</h1>");
-  fs.readFile("./view/index.html", (err, data) => {
-    if (err) {
-      console.log(err);
-      res.end();
-    } else {
-      res.write(data);
-      res.end();
-    }
-  });
-  res.end();
+app.get("/", (req, res) => {
+  res.sendFile(`./views/index.html`, { root: __dirname });
 });
 
-server.listen(port, hostname, () => {
-  console.log(`your server is runnig  http://localhost:${port}`);
+app.get("/contact", (req, res) => {
+  res.sendFile(`./views/contact.html`, { root: __dirname });
 });
+
+app.use((req, res) => {
+  res.sendFile(`./views/404.html`, { root: __dirname });
+});
+app.listen(port, hostname, () => {
+  console.log(`your server is listing http://localhost:${port}`);
+});
+
+// const server = http.createServer((req, res) => {
+//   res.setHeader("Content-Type", "text/html");
+//   let route = "./views";
+//   switch (res.url) {
+//     case "/":
+//       route += "index.html";
+//       res.statusCode = 200;
+//       break;
+//     case "/contact.html":
+//       route += "contact.html";
+//       res.statusCode = 200;
+//       break;
+//     case "/404":
+//       route += "404.html";
+//       res.statusCode = 200;
+//       break;
+//   }
+//   res.write("<h1>hello world</h1>");
+//   fs.readFile(route, (err, data) => {
+//     if (err) {
+//       console.log(err);
+//       res.end();
+//     } else {
+//       res.write(data);
+//       res.end(data);
+//     }
+//   });
+//   res.end();
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`your server is runnig  http://localhost:${port}`);
+// });
