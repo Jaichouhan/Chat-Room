0f9316ea2fd15a6180053cdd692ba2cc19092f7a
Jai chouhan <92376924+Jaichouhan@users.noreply.github.com>
2022-08-04 12:57:36 +0530

    delete

--- data/app/server.js
+++ data/app/server.js
@@ -1,4 +1,3 @@
-// const http = require("http");
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
@@ -8,6 +7,7 @@ const mongodb =
 // const fs = require("fs");
 const port = 8000;
 const hostname = "localhost";
+app.use(express.urlencoded({ extended: true }));
 
 const data = require("./modules/item");
 
@@ -24,87 +24,79 @@ mongoose
   });
 
 app.get("/", (req, res) => {
-  //   res.sendFile(`./views/index.ejs`, { root: __dirname });
-  const items = [
-    {
-      name: "jon doa",
-      price: 150,
-    },
-    {
-      name: "ram doa",
-      price: 50,
-    },
-    {
-      name: "honey doa",
-      price: 25,
-    },
-  ];
-  res.render("index", { items });
+  res.redirect("/get-items");
 });
 
-app.get("/create-item", (req, res) => {
-  const item = new data({
-    name: "computer",
-    price: 10000,
-  });
-  item
-    .save()
-    .then((res) => console.log(res))
-    .catch((err) => console.log(err));
-});
+// app.get("/create-item", (req, res) => {
+//   const item = new data({
+//     name: "computer",
+//     price: 10000,
+//   });
+//   item
+//     .save()
+//     .then((res) => console.log(res))
+//     .catch((err) => console.log(err));
+// });
 app.get("/get-items", (req, res) => {
   data
     .find()
-    .then((res) => console.log(res))
+    .then((data) => {
+      res.render("index", { items: data });
+    })
     .catch((err) => console.log(err));
 });
-app.get("/get-item", (req, res) => {
+// app.get("/get-item", (req, res) => {
+//   data
+//     .findById("62ea657795fe3767e1e3001c")
+//     .then((res) => console.log(res))
+//     .catch((err) => console.log(err));
+// });
+
+app.post("/items", (req, res) => {
+  console.log(req.body);
+  const item = data(req.body);
+  item
+    .save()
+    .then(() => {
+      res.redirect("/get-items");
+    })
+    .catch((err) => console.log(err));
+});
+
+app.get("/items/:id", (req, res) => {
+  const id = req.params.id;
   data
-    .findById("62ea657795fe3767e1e3001c")
-    .then((res) => console.log(res))
+    .findById(id)
+    .then((result) => {
+      res.render("item-deatils", { item: result });
+    })
+    .catch((err) => console.log(err));
+});
+
+app.delete("/items/:id", (req, res) => {
+  const id = req.params.id;
+  data
+    .findByIdAndDelete(id)
+    .then((result) => {
+      res.json({ redirect: "/get-items" });
+    })
+    .catch((err) => console.log(err));
+});
+
+app.put("/items/:id", (req, res) => {
+  const id = req.params.id;
+  data
+    .findByIdAndUpdate(id, req.body)
+    .then((result) => {
+      res.json({ msg: "updated successfully " });
+    })
     .catch((err) => console.log(err));
 });
 
 app.get("/contact", (req, res) => {
-  //   res.sendFile(`./views/contact.ejs`, { root: __dirname });
   res.render("contact");
 });
 
 app.use((req, res) => {
-  //   res.sendFile(`./views/404.ejs`, { root: __dirname });
   res.render("404");
 });
-
-// const server = http.createServer((req, res) => {
-//   res.setHeader("Content-Type", "text/html");
-//   let route = "./views";
-//   switch (res.url) {
-//     case "/":
-//       route += "index.html";
-//       res.statusCode = 200;
-//       break;
-//     case "/contact.html":
-//       route += "contact.html";
-//       res.statusCode = 200;
-//       break;
-//     case "/404":
-//       route += "404.html";
-//       res.statusCode = 200;
-//       break;
-//   }
-//   res.write("<h1>hello world</h1>");
-//   fs.readFile(route, (err, data) => {
-//     if (err) {
-//       console.log(err);
-//       res.end();
-//     } else {
-//       res.write(data);
-//       res.end(data);
-//     }
-//   });
-//   res.end();
-// });
-
-// server.listen(port, hostname, () => {
-//   console.log(`your server is runnig  http://localhost:${port}`);
-// });
