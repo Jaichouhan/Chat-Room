f36edc83aca78159b22c15f5b78b22ee6dc17874
Jai chouhan <92376924+Jaichouhan@users.noreply.github.com>
2022-10-04 18:18:27 +0530

    done status

--- data/app/server.js
+++ data/app/server.js
@@ -37,6 +37,7 @@ app.get("/", (req, res) => {
 //     .catch((err) => console.log(err));
 // });
 console.log(done, "done");
+console.log(done, "done");
 app.get("/get-items", (req, res) => {
   data
     .find()
